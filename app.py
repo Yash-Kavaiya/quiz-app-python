0040dd88958fb7e067b7beb8bbc226693f0245cf
Yash Kavaiya <103239923+Yash-Kavaiya@users.noreply.github.com>
2023-10-09 15:49:25 +0000

    Add login page

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route('/')
+def results():
+    answers = request.args
+    return render_template('index.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+if __name__ == '__main__':
+    app.run(debug=True)
